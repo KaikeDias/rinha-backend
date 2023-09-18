e1467c0525867ee2d4748426a4c15e2836dcf787
Kaike <kaikedias98@gmail.com>
2023-09-18 00:27:19 -0300

    feat: add Person DTO

---
+++ src/main/java/com/example/rinhabackend/app/DTOs/PersonDTO.java
@@ -0,0 +1,23 @@
+package com.example.rinhabackend.app.DTOs;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+public record PersonDTO(
+   @NotNull
+   @Size(max = 100)
+   String name,
+
+   @NotNull
+   @Size(max = 32)
+   String nickname,
+
+   @NotNull
+   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+   String birthday,
+
+   List<@Size(min = 1, max = 32)String> stack
+) {}
