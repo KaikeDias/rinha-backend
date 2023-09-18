31536a47ac9873496c2f4313e60ca9b62ffd9ce6
Kaike <kaikedias98@gmail.com>
2023-09-18 02:52:31 -0300

    feat: add method existsByNickname

--- src/main/java/com/example/rinhabackend/app/repositories/PersonRepository.java
+++ src/main/java/com/example/rinhabackend/app/repositories/PersonRepository.java
@@ -4,7 +4,10 @@ import com.example.rinhabackend.app.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Person, UUID> {}
+public interface PersonRepository extends JpaRepository<Person, UUID> {
+    Boolean existsByNickname(String nickname);
+}
