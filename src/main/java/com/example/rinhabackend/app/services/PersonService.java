f7191a6ec4a09fc08c56d057422aef94148be612
Kaike <kaikedias98@gmail.com>
2023-09-18 02:52:03 -0300

    feat: add methods findById, findByTerm and countPersons

--- src/main/java/com/example/rinhabackend/app/services/PersonService.java
+++ src/main/java/com/example/rinhabackend/app/services/PersonService.java
@@ -4,9 +4,12 @@ import com.example.rinhabackend.app.DTOs.PersonDTO;
 import com.example.rinhabackend.app.entities.Person;
 import com.example.rinhabackend.app.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PersonService {
@@ -22,6 +25,10 @@ public class PersonService {
     }
 
     public Person savePerson(PersonDTO personDTO) {
+        if(personRepository.existsByNickname(personDTO.nickname())) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Esse apelido ja esta em uso");
+        }
+
         var person = new Person();
         person.setName(personDTO.name());
         person.setNickname(personDTO.nickname());
@@ -31,5 +38,27 @@ public class PersonService {
         personRepository.save(person);
 
         return person;
+    }
+
+    public Person findPersonById(UUID id) {
+        var person = personRepository.findById(id);
+
+        if(person.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pessoa nao encontrada");
+        }
+
+        return person.get();
+    }
+
+    public List<Person> findPersonsByTerm(String term) {
+        return personRepository
+                .findAll()
+                .stream()
+                .filter(people -> people.getName().toLowerCase().contains(term) | people.getNickname().toLowerCase().contains(term) | people.getStackList().contains(term))
+                .toList();
+    }
+
+    public Long countPersons() {
+        return personRepository.count();
     }
 }
