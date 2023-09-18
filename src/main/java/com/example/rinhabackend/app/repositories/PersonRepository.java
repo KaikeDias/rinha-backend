d0823d69a24eff81b0729f5bb8b6480cd0efa051
Kaike <kaikedias98@gmail.com>
2023-09-18 00:28:03 -0300

    feat: add Person repository

---
+++ src/main/java/com/example/rinhabackend/app/repositories/PersonRepository.java
@@ -0,0 +1,10 @@
+package com.example.rinhabackend.app.repositories;
+
+import com.example.rinhabackend.app.entities.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, UUID> {}
