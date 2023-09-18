9372b046aab87f575cb6cf3a3f2f96eab4bc299c
Kaike <kaikedias98@gmail.com>
2023-09-18 02:53:35 -0300

    feat: add methods getOnePersonById, GetAllPersonsByTerm and countPersons

--- src/main/java/com/example/rinhabackend/api/controllers/PersonController.java
+++ src/main/java/com/example/rinhabackend/api/controllers/PersonController.java
@@ -3,14 +3,14 @@ package com.example.rinhabackend.api.controllers;
 import com.example.rinhabackend.app.DTOs.PersonDTO;
 import com.example.rinhabackend.app.services.PersonService;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
 
 @RestController
 public class PersonController {
@@ -31,14 +31,38 @@ public class PersonController {
         }
     }
 
-    @GetMapping("/pessoas")
-    public ResponseEntity<Object> getAllPersons() {
+//    @GetMapping("/pessoas")
+//    public ResponseEntity<Object> getAllPersons() {
+//        try {
+//            var persons = personService.findAll();
+//            return ResponseEntity.ok(persons);
+//        } catch (ResponseStatusException ex) {
+//            return ResponseEntity.status(ex.getStatusCode()).body(ex.getReason());
+//        }
+//    }
+
+    @GetMapping("/pessoas/{id}")
+    public ResponseEntity<Object> getOnePersonById(@PathVariable UUID id) {
         try {
-            var persons = personService.findAll();
+            var person = personService.findPersonById(id);
+            return ResponseEntity.ok(person);
+        } catch (ResponseStatusException ex) {
+            return ResponseEntity.status(ex.getStatusCode()).body(ex.getReason());
+        }
+    }
+
+    @GetMapping("/pessoas")
+    public ResponseEntity<Object> getAllPersonsByTerm(@RequestParam(value = "t") String term) {
+        try {
+            var persons = personService.findPersonsByTerm(term);
             return ResponseEntity.ok(persons);
         } catch (ResponseStatusException ex) {
             return ResponseEntity.status(ex.getStatusCode()).body(ex.getReason());
         }
     }
 
+    @GetMapping("/contagem-pessoas")
+    public ResponseEntity<Long> countPersons() {
+        return ResponseEntity.ok(personService.countPersons());
+    }
 }
